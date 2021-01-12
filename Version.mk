68722f28e895a191824f3df6dae3e21e4d5480f7
cgsfv <cgsfv@users.noreply.github.com>
2021-01-12 11:13:10 -0800

    Bumping version for release

--- Version.mk
+++ Version.mk
@@ -2,7 +2,7 @@
 XC3SPROG_VERSION := 0.1.2
 
 # Customization ID, which should identify the customization added to the original by SiFive
-FREEDOM_XC3SPROG_ID := 2020.12.0-preview1
+FREEDOM_XC3SPROG_ID := 2020.12.0
 
 # Characteristic tags, which should be usable for matching up providers and consumers
 FREEDOM_XC3SPROG_RISCV_TAGS = 
