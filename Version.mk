fe370e33649fd6a0020640924a403091e8371af5
cgsfv <cgsfv@users.noreply.github.com>
2021-01-21 10:35:14 -0800

    Bumping version and fixing dll discovery for new mingw

--- Version.mk
+++ Version.mk
@@ -2,7 +2,7 @@
 XC3SPROG_VERSION := 0.1.2
 
 # Customization ID, which should identify the customization added to the original by SiFive
-FREEDOM_XC3SPROG_ID := 2020.12.0
+FREEDOM_XC3SPROG_ID := 2020.12.1
 
 # Characteristic tags, which should be usable for matching up providers and consumers
 FREEDOM_XC3SPROG_RISCV_TAGS = 
